267e9ef94857fe3619ee7669db8b5a9b89ffaeed
FernandoRojas07 <lfernandorojasf@gmail.com>
2015-10-01 00:27:40 -0500

    Update CasaP.java

--- CasaP.java
+++ CasaP.java
@@ -1,5 +1,5 @@
 
-class CasaP
+/*class CasaP
 {
    private DireccionP dir;
    int indice;
@@ -16,5 +16,23 @@ class CasaP
    }
    public void LeeSueldo(){
    sueldo=Teclado.LeeDouble("Cual es el ingreso total de la casa: ");
+   }
+}
+*/
+class CasaP
+{
+   private DireccionP dir;
+   PersonaP []casas;
+   int indice,noper,contperso=0;
+   public void CasasCap(int per)
+   {
+      casas= new PersonaP[per];
+      System.out.println("Hola");
+      for(indice=0;indice<per;indice++)
+      {  
+         noper=Teclado.LeeEntero("¿Cuántas personas hay en el hogar?");
+         casas[indice].PersonaP(noper);
+         contperso=contperso+noper;
+      }
    }
 }
